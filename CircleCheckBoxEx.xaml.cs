754577032eb441721d459b31e4f5f283448e25f3
saiu <SNsaiu@outlook.com>
2022-09-30 17:23:34 +0800

    添加控件

---
+++ CircleCheckBoxEx.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Fantasy.Maui.Controls.Animation;
+namespace Fantasy.Maui.Controls;
+
+public partial class CircleCheckBoxEx : ContentView
+{
+	public CircleCheckBoxEx()
+	{
+		InitializeComponent();
+		if (this.IsChecked)
+			this.thumb.FadeTo(1);
+		else
+			this.thumb.FadeTo(0);
+	}
+
+	public static BindableProperty IsCheckedProperty = BindableProperty.Create(
+		"IsChecked",
+		typeof(bool),
+		typeof(CircleCheckBoxEx),
+		defaultValue:false
+
+		);
+
+	public bool IsChecked
+	{
+		get { return (bool)GetValue(IsCheckedProperty); }
+		set { SetValue(IsCheckedProperty, value); }
+	}
+
+	public static BindableProperty TextProperty = BindableProperty.Create(
+		"Text",
+		typeof(string),
+		typeof(CircleCheckBoxEx),
+		defaultValue: ""
+		);
+
+	public string Text
+	{
+		get { return (string)GetValue(TextProperty); }
+		set { SetValue(TextProperty, value); }
+	}
+
+
+    void TapGestureRecognizer_Tapped(System.Object sender, Microsoft.Maui.Controls.TappedEventArgs e)
+    {
+		this.IsChecked = !this.IsChecked;
+		if(!this.IsChecked)
+		{
+			this.thumb.FadeTo(0);
+		
+		}
+		else
+		{
+			this.thumb.FadeTo(1);
+		}
+        this.thumb.OnceAninmation(TransformType.Scale, 1.4, 1, 200, Easing.Default);
+    }
+}
